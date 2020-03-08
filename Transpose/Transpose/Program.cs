319e26314dddbeb7a110b74e31eb63bf628e1c8f
kamil <48632268+kamil-reczek@users.noreply.github.com>
2020-03-08 10:39:36 +0100

    Matrix transposition: addind time and operations measurements

--- Transpose/Transpose/Program.cs
+++ Transpose/Transpose/Program.cs
@@ -6,13 +6,18 @@ namespace Transpose
     {
         static void Main(string[] args)
         {
-            int[,] m = Transpose.readMatrix();
-            Transpose.displayMatrix(m);
-            Transpose.displayMatrix(Transpose.transposeMatrix(m));
-
-#if DEBUG
-            Console.ReadKey();
-#endif
+            // Testing if matrix generation and transposition works
+            int[,] test = Transpose.generateIntMatrix(2,5);
+            Transpose.displayMatrix(test);
+            Transpose.displayMatrix(Transpose.transposeMatrix(test));
+            
+            // Measuring transposition complexity for matrices size 1k - 10k
+            for (int i = 1000; i <= 10000; i += 500)
+            {
+                int[,] m = Transpose.generateIntMatrix(i, i);
+                Console.WriteLine(Transpose.measureTime(m));
+                Console.WriteLine(Transpose.measureOperations(m));
+            }
         }
     }
 }
--- Transpose/Transpose/Transpose.cs
+++ Transpose/Transpose/Transpose.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 
 namespace Transpose
 {
@@ -17,14 +18,25 @@ namespace Transpose
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    Console.Write("Procide elemnt [" + i + ", " + j + "] = ");
+                    Console.Write(@"Provide element [{i},{j}] = ");
                     result[i, j] = int.Parse(Console.ReadLine());
                 }
             }
             return result;
 
         }
-
+        
+        public static int[,] generateIntMatrix(int rows, int cols) 
+        {
+            if (rows == 0 || cols == 0) throw new ArgumentOutOfRangeException("Cols and rows need to be > 0");
+            Random rand = new Random();
+            int[,] matrix = new int[rows, cols];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                matrix[i, j] = rand.Next(25);
+            return matrix;
+        }
+        
         public static void displayMatrix(int[,] m)
         {
             Console.WriteLine("Displaying matrix: ");
@@ -32,7 +44,7 @@ namespace Transpose
             {
                 for (int j = 0; j < m.GetLength(1); j++)
                 {
-                    Console.Write(m[i, j] + " ");
+                    Console.Write($"{m[i, j]}\t");
                 }
                 Console.WriteLine();
             }
@@ -40,7 +52,6 @@ namespace Transpose
 
         public static int[,] transposeMatrix(int[,] m)
         {
-            Console.WriteLine("Transposing matrix ...");
             int[,] t = new int[m.GetLength(1), m.GetLength(0)];
             for (int i = 0; i < m.GetLength(0); i++)
             {
@@ -51,6 +62,55 @@ namespace Transpose
             }
 
             return t;
+        }
+
+        public static int[,] transposeMatrix(int[,] m, out long counter)
+        {
+            counter = 0;
+            int[,] t = new int[m.GetLength(1), m.GetLength(0)];
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    t[j, i] = m[i, j];
+                    counter++;
+                }
+            }
+            return t;
+        }
+
+        public static string measureTime(int[,] m)
+        {
+            Stopwatch st = new Stopwatch();
+            int size = m.GetLength(0);
+            int sampleSize = 10;
+            long sampleTime = 0;
+            long totalTime = 0;
+            long minTime = long.MaxValue;
+            long maxTime = long.MinValue;
+            
+            for (int i = 0; i < sampleSize + 2; i++)
+            {
+                st.Reset();
+                st.Start();
+                transposeMatrix(m);
+                st.Stop();
+                sampleTime = st.ElapsedMilliseconds;
+                totalTime += sampleTime;
+                if (minTime > sampleSize) minTime = sampleSize;
+                if (maxTime < sampleSize) maxTime = sampleSize;
+            }
+
+            long result = (totalTime - minTime - maxTime) / sampleSize;
+            return $"{size},time,{result}";
+        }
+
+        public static string measureOperations(int[,] m)
+        {
+            int size = m.GetLength(0);
+            long counter = 0;
+            transposeMatrix(m, out counter);
+            return $"{size},counter,{counter}";
         }
     }
 }
