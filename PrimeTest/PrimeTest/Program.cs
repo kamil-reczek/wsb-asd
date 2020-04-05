b0a4731fb11bd10f236b77587cebaaaf9a49a831
kamil <48632268+kamil-reczek@users.noreply.github.com>
2020-04-05 14:51:53 +0200

    Adding example prime test for Project 2

---
+++ PrimeTest/PrimeTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+
+namespace PrimeTest
+{
+    class Program
+    {
+        
+        private static bool IsPrimeExample(BigInteger n)
+        {
+            if (n < 2) return false;
+            else if (n < 4) return true;
+            else if (n % 2 == 0) return false;
+            else
+            {
+                for (BigInteger i = 3; i < n / 2; i += 2)
+                {
+                    if (n % i == 0) return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool IsPrimeDecent(BigInteger n) {
+            // TODO: Implement the 'decent' version of example prime test
+            return true;
+        }
+        
+        static void Main(string[] args)
+        {
+            // Testing for small number
+            for (int i = 1; i < 100; i++)
+            {
+                Console.WriteLine($"Number {i} is prime: {IsPrimeExample(i)}");
+            }
+            
+        }
+    }
+}
